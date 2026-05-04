ab056e7ac49053c99ada878a32fae7f8112cc4e9
visibyltech <contact@visibyl.biz>
2026-05-04 13:50:43 +0100

    feat: wrap app in ThemeProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,66 +1,65 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-  import { BrowserRouter, Route, Routes } from "react-router-dom";
-  import { Toaster as Sonner } from "@/components/ui/sonner";
-  import { Toaster } from "@/components/ui/toaster";
-  import { TooltipProvider } from "@/components/ui/tooltip";
-  import { AuthProvider } from "@/contexts/AuthContext";
-  import { CurrencyProvider } from "@/contexts/CurrencyContext";
-  import { CartProvider } from "@/contexts/CartContext";
-  import { WishlistProvider } from "@/contexts/WishlistContext";
-  import { ThemeProvider } from "@/contexts/ThemeContext";
-  import Index from "./pages/Index.tsx";
-  import Shop from "./pages/Shop.tsx";
-  import ProductDetail from "./pages/ProductDetail.tsx";
-  import Cart from "./pages/Cart.tsx";
-  import Wishlist from "./pages/Wishlist.tsx";
-  import Auth from "./pages/Auth.tsx";
-  import Account from "./pages/Account.tsx";
-  import Checkout from "./pages/Checkout.tsx";
-  import OrderSuccess from "./pages/OrderSuccess.tsx";
-  import Orders from "./pages/Orders.tsx";
-  import About from "./pages/About.tsx";
-  import Admin from "./pages/Admin.tsx";
-  import ResetPassword from "./pages/ResetPassword.tsx";
-  import NotFound from "./pages/NotFound.tsx";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Toaster as Sonner } from "@/components/ui/sonner";
+import { Toaster } from "@/components/ui/toaster";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { AuthProvider } from "@/contexts/AuthContext";
+import { CurrencyProvider } from "@/contexts/CurrencyContext";
+import { CartProvider } from "@/contexts/CartContext";
+import { WishlistProvider } from "@/contexts/WishlistContext";
+import { ThemeProvider } from "@/contexts/ThemeContext";
+import Index from "./pages/Index.tsx";
+import Shop from "./pages/Shop.tsx";
+import ProductDetail from "./pages/ProductDetail.tsx";
+import Cart from "./pages/Cart.tsx";
+import Wishlist from "./pages/Wishlist.tsx";
+import Auth from "./pages/Auth.tsx";
+import Account from "./pages/Account.tsx";
+import Checkout from "./pages/Checkout.tsx";
+import OrderSuccess from "./pages/OrderSuccess.tsx";
+import Orders from "./pages/Orders.tsx";
+import About from "./pages/About.tsx";
+import Admin from "./pages/Admin.tsx";
+import ResetPassword from "./pages/ResetPassword.tsx";
+import NotFound from "./pages/NotFound.tsx";
 
-  const queryClient = new QueryClient();
+const queryClient = new QueryClient();
 
-  const App = () => (
-    <QueryClientProvider client={queryClient}>
-      <ThemeProvider>
-        <TooltipProvider>
-          <BrowserRouter>
-            <AuthProvider>
-              <CurrencyProvider>
-                <WishlistProvider>
-                  <CartProvider>
-                    <Toaster />
-                    <Sonner />
-                    <Routes>
-                      <Route path="/" element={<Index />} />
-                      <Route path="/shop" element={<Shop />} />
-                      <Route path="/product/:slug" element={<ProductDetail />} />
-                      <Route path="/cart" element={<Cart />} />
-                      <Route path="/wishlist" element={<Wishlist />} />
-                      <Route path="/checkout" element={<Checkout />} />
-                      <Route path="/order-success" element={<OrderSuccess />} />
-                      <Route path="/auth" element={<Auth />} />
-                      <Route path="/account" element={<Account />} />
-                      <Route path="/account/orders" element={<Orders />} />
-                      <Route path="/about" element={<About />} />
-                      <Route path="/admin" element={<Admin />} />
-                      <Route path="/reset-password" element={<ResetPassword />} />
-                      <Route path="*" element={<NotFound />} />
-                    </Routes>
-                  </CartProvider>
-                </WishlistProvider>
-              </CurrencyProvider>
-            </AuthProvider>
-          </BrowserRouter>
-        </TooltipProvider>
-      </ThemeProvider>
-    </QueryClientProvider>
-  );
+const App = () => (
+  <QueryClientProvider client={queryClient}>
+    <ThemeProvider>
+      <TooltipProvider>
+        <BrowserRouter>
+          <AuthProvider>
+            <CurrencyProvider>
+              <WishlistProvider>
+                <CartProvider>
+                  <Toaster />
+                  <Sonner />
+                  <Routes>
+                    <Route path="/" element={<Index />} />
+                    <Route path="/shop" element={<Shop />} />
+                    <Route path="/product/:slug" element={<ProductDetail />} />
+                    <Route path="/cart" element={<Cart />} />
+                    <Route path="/wishlist" element={<Wishlist />} />
+                    <Route path="/checkout" element={<Checkout />} />
+                    <Route path="/order-success" element={<OrderSuccess />} />
+                    <Route path="/auth" element={<Auth />} />
+                    <Route path="/account" element={<Account />} />
+                    <Route path="/account/orders" element={<Orders />} />
+                    <Route path="/about" element={<About />} />
+                    <Route path="/admin" element={<Admin />} />
+                    <Route path="/reset-password" element={<ResetPassword />} />
+                    <Route path="*" element={<NotFound />} />
+                  </Routes>
+                </CartProvider>
+              </WishlistProvider>
+            </CurrencyProvider>
+          </AuthProvider>
+        </BrowserRouter>
+      </TooltipProvider>
+    </ThemeProvider>
+  </QueryClientProvider>
+);
 
-  export default App;
-  
+export default App;
